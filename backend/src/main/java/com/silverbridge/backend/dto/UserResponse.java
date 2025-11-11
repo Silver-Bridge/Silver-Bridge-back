875a729df8d3c82fca0f5e28bd6768f91a814cf9
최민서 <114375668+oneokiwa@users.noreply.github.com>
2025-11-11 23:37:18 +0900

    Feat: UserResponse dto 추가

---
+++ backend/src/main/java/com/silverbridge/backend/dto/UserResponse.java
@@ -0,0 +1,32 @@
+package com.silverbridge.backend.dto;
+
+import com.silverbridge.backend.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserResponse {
+
+	private Long id;
+	private String name;
+	private String phoneNumber;
+	private String birth;
+	private Boolean gender;
+	private String region;
+	private String textsize;
+	private Boolean social;
+
+	public static UserResponse from(User user) {
+		return UserResponse.builder()
+				.id(user.getId())
+				.name(user.getName())
+				.phoneNumber(user.getPhoneNumber())
+				.birth(user.getBirth())
+				.gender(user.getGender())
+				.region(user.getRegion())
+				.textsize(user.getTextsize())
+				.social(user.getSocial())
+				.build();
+	}
+}
